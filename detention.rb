e4b6f79c0ca09cba55fb44b3bcc7f63365deef4d
sirikops <sirikops@hotmail.com>
2018-06-07 21:41:21 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -1,6 +1,7 @@
 #Code your detention class here
 class Detention
   attr_accessor :first_name, :last_name, :age, :grade
+  
 def initialize(first_name, last_name, age, grade)
   @first_name=first_name
   @last_name=last_name
